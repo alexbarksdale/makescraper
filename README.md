1a502a1b4119fe849c5a39237ee7c882d58730e3
alexbarksdale <ale.barksdale@gmail.com>
2020-06-25 15:44:00 -0500

    [Add] Print news to console and serialize to json

--- README.md
+++ README.md
@@ -43,9 +43,9 @@ _Complete each task in the order they appear. Use [GitHub Task List](https://hel
 #### Scraping
 
 - [X] **IMPORTANT**: Complete the Web Scraper Workflow worksheet distributed in class.
-- [ ] Create a `struct` to store your data.
-- [ ] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
-- [ ] Print the data you scraped to `stdout`.
+- [X] Create a `struct` to store your data.
+- [X] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
+- [X] Print the data you scraped to `stdout`.
 
 ##### Stretch Challenges
 
@@ -53,7 +53,7 @@ _Complete each task in the order they appear. Use [GitHub Task List](https://hel
 
 #### Serializing & Saving
 
-- [ ] Serialize the `struct` you created to JSON. Print the JSON to `stdout` to validate it.
+- [X] Serialize the `struct` you created to JSON. Print the JSON to `stdout` to validate it.
 - [ ] Write scraped data to a file named `output.json`.
 - [ ] **Add, commit, and push to GitHub**.
 
--- scrape.go
+++ scrape.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/gocolly/colly"
 )
 
 type newsItem struct {
-	title   string
-	summary string
-	tag     string
+	Title   string
+	Summary string
+	Tag     string
+}
+
+func printNews(n []newsItem) {
+	fmt.Println("\n-- NEWS FOR TODAY --")
+	for _, item := range n {
+		fmt.Printf("Title: %v\nSummary: %v\nTag: %v\n\n", item.Title, item.Summary, item.Tag)
+	}
+}
+
+func serializeToJSON(n []newsItem) {
+	fmt.Println("Serializing news to JSON...")
+	for i := range n {
+		serialized, _ := json.Marshal(n[i])
+		fmt.Println(string(serialized))
+	}
 }
 
 func main() {
@@ -20,17 +36,18 @@ func main() {
 	// On every a element which has href attribute call callback
 	c.OnHTML(".module--news ul > li", func(e *colly.HTMLElement) {
 		item := newsItem{}
-		item.title = e.ChildText(".media__title")
-		item.summary = e.ChildText(".media__summary")
-		item.tag = e.ChildText(".media__tag")
+		item.Title = e.ChildText(".media__title")
+		item.Summary = e.ChildText(".media__summary")
+		item.Tag = e.ChildText(".media__tag")
 		news = append(news, item)
 	})
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
+		fmt.Printf("Looking up the news for today... \nProvider: %v\n", r.URL.String())
 	})
 
 	c.Visit("https://www.bbc.com/")
-	fmt.Println("News: ", news[0])
+	printNews(news)
+	serializeToJSON(news)
 }
